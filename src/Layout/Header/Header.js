f516de264d10da0fe14b54dade9863537cdf97c6
Darko <darko.bogovic92@gmail.com>
2019-03-11 00:07:22 +0100

    adding header component and styling logo

---
+++ src/Layout/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo-box">
+        <h1 className="title">
+          <span>Online</span>
+          <span>Shopping</span>
+        </h1>
+      </div>
+      <div>
+      
+      </div>
+      <div>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Header;

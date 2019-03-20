e52409745edf5949f2b9a617373af812ef03674b
Darko <darko.bogovic92@gmail.com>
2019-03-20 23:44:08 +0100

    adding products from json file to product page

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import './products.css';
+import ProductsList from './productsList.json';
 
 class Products extends Component {
   render() {
+    const list = ProductsList.map((item) => {
+      return(
+        <div className="product">
+          <img src={item.src} className="product-image" />
+          <h4 className="product-description">
+            <span>{item.name}</span>
+            <span>{item.price}</span>
+          </h4>
+        </div>
+      );
+    })
     return (
       <div className="products-container">
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
-        <div className="products"></div>
+        { list }
       </div>
     );
   }
